2173cd1935e71b404d7eecb991d446d25613ce4c
SpectralBurger <120690971+SpectralBurger@users.noreply.github.com>
2026-06-13 00:10:36 +0300

    Create optimizer.py

---
+++ ML_Solver/optimizer.py
@@ -0,0 +1,9 @@
+import pymoo 
+import numpy as np
+
+
+from pymoo.algorithms.moo.nsga3 import NSGA3
+from pymoo.optimize import minimize
+from pymoo.problems import get_problem
+from pymoo.util.ref_dirs import get_reference_directions
+
