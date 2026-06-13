9df941e2d185ac520f7ff6a1ceebc6770f8f67c9
SpectralBurger <120690971+SpectralBurger@users.noreply.github.com>
2026-06-14 00:57:56 +0300

    problem for NSGA3, not complete

---
+++ ML_Solver/suspension_problem.py
@@ -0,0 +1,23 @@
+import numpy as np
+from pymoo.core.problem import ElementwiseProblem
+
+class SuspensionProblem(ElementwiseProblem):
+    def __init__(self):
+        # number of variables being optimized, in x,y,z for each coordinate based hardpoint (since tyre data is fixed that doesnt count)
+        n_var = 30
+        # number of objectives (for example camber change, caster change, etc.)
+        n_obj = 12
+        # number of constraints, these are objectives the solver MUST achieve exactly
+        n_con = 0
+
+        # Initial hardpoints go here (x y and z from origin in mm)
+        initial_hardpoints = np.array([1.0,2.0,3.0]) #placeholders for now
+        # Define bounding boxes in the x, y and z direction
+        xl = initial_hardpoints - 20
+        xu = initial_hardpoints + 20
+        super().__init__(n_var=n_var, n_obj=n_obj, n_constr=n_con, xl=xl, xu=xu)
+    
+    def _evaluate(self, x, out, *args, **kwargs):
+
+        
+      
