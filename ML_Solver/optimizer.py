749ebe875131e3931c2190541d76ad7d47b82ce9
SpectralBurger <120690971+SpectralBurger@users.noreply.github.com>
2026-07-05 22:38:41 +0300

    Update optimizer.py

--- ML_Solver/optimizer.py
+++ ML_Solver/optimizer.py
@@ -15,6 +15,13 @@ from pymoo.mcdm.pseudo_weights import PseudoWeights
 
 
 def main():
+    # ===========================
+    # ======== VARIABLES ========
+    # ===========================
+
+    # Set the location to wherever your geometry yaml and sweep files are. VERY IMPORTANT. CODE WILL BREAK IF YOU DO NOT UPDATE THIS.
+    geometry_yaml = r"C:\Users\adwai\Desktop\Skool\Local Git Repos\SuspensionHardPointsProject\ML_Solver\data\geometry.yaml" 
+    sweep_yaml = r"C:\Users\adwai\Desktop\Skool\Local Git Repos\SuspensionHardPointsProject\ML_Solver\data\sweep.yaml"
 
     # Labels for points and objectives in order. Changing this will only change the labels, you need to go into suspension.py to actually change the objectives/points
     HARDPOINTS = [
@@ -35,20 +42,40 @@ def main():
 
     # Values for objectives in order. Changing this WILL affect the values of the objectives, in the order given in suspension_problem. Units mm and degrees.
     OBJECTIVE_VALUES = [0, 0, 0, 10, 38.1, 0, 0]
+
     # Weights in same order
     weights = np.array([0.22, 0.22, 0.22, 0.04, 0.04, 0.22, 0.04])
+
     # Length of side of cube in which optimizer will search for each point.
     cube_side_length_mm = 30 #mm
 
-    # Set the location to wherever your geometry yaml and sweep files are. VERY IMPORTANT
-    geometry_yaml = r"C:\Users\adwai\Desktop\Skool\Local Git Repos\SuspensionHardPointsProject\ML_Solver\data\geometry.yaml" 
-    sweep_yaml = r"C:\Users\adwai\Desktop\Skool\Local Git Repos\SuspensionHardPointsProject\ML_Solver\data\sweep.yaml"
+    # Number of generations
+    gen_amount = 100
 
     n_procs = 20 # number of logical processors, check how many you have available before running, using cntl + shift + esc and checking cpu logical processors amount.
     #run about 3-4 processors lower than what is available. You can run max but your cpu will be fully occupied. The lower the number of cores, the longer it will take.
+
+    # create the reference directions to be used for the optimization (NSGA and its variants only) First argument is the algorithm, second is the number of directions (objectives), third is the number of samples.
+    ref_dirs = get_reference_directions("energy", 7, 100)  
+
+    # The algorithm the optimizer will use. Note that changing this may require the main code to be updated if the new algorithm has any new parameters.
+    algorithm = UNSGA3(ref_dirs=ref_dirs)
+
+    # How the results are written to the file. "w" will clear the file each run and output the new points only, "a" will append the new points to the end of the file and not clear old results.
+    writemode = "a"
+
+    # ===========================
+    # ======== MAIN CODE ========
+    # ===========================
+
+    # Set up CPU parallelization
     pool = Pool(n_procs)
     runner = StarmapParallelization(pool.starmap)
 
+    # Create the termination condition
+    termination = get_termination("n_gen", gen_amount)
+
+    # Create the problem to be run in the optimizer
     problem = SuspensionProblem(
         geometry_path=geometry_yaml, 
         sweep_path=sweep_yaml,
@@ -56,18 +83,11 @@ def main():
         objective_values=OBJECTIVE_VALUES,
         elementwise_runner=runner
     )
-
-    # create the reference directions to be used for the optimization (NSGA and its variants only)
-    ref_dirs = get_reference_directions("energy", 7, 100)
     
-    # create the algorithm object
-    algorithm = UNSGA3(ref_dirs=ref_dirs)
-    
-    # increase the number here if you want it to run more generations
-    termination = get_termination("n_gen", 2)
 
-    # Loop code starts here
     print("Starting optimization loop...")
+
+    #  Run the optimizer
     res = minimize(
         problem,
         algorithm,
@@ -76,19 +96,28 @@ def main():
         verbose=True     # This prints generation progress in your terminal
     )
 
+    # Close the pool
     pool.close()
     pool.join()
+
+    # ===========================
+    # ====== POST-PROCESS =======
+    # ===========================
+
+    # Get the Pareto front (set of best points) and associated metrics
     points_pareto = res.X
     objectives_pareto = res.F
 
+    # Get the id of the best points according to weights
     best_idx = PseudoWeights(weights).do(objectives_pareto)
 
+    # Get the best point according to weights
     best_suspension_geometry = points_pareto[best_idx]
     best_suspension_metrics = objectives_pareto[best_idx]
 
 
-    # Print to file
-    with open("results.txt", "a") as f:
+    # Print results to file. Includes entire pareto front, and the best overall point (bottom of file) 
+    with open("results.txt", writemode) as f:
         f.write(f"=== Entire Pareto Front ({len(points_pareto)} Configurations) ===\n")
         
 
